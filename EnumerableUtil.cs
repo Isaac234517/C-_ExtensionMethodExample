37e226f94dbe8c9ec7033cd055ae536d4f16f6e7
isaac <isaac234517@gmail.com>
2018-01-04 23:34:25 +0800

    completed Extension Method example

---
+++ EnumerableUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExample
+{
+    public static class EnumerableUtil
+    {
+        public static IEnumerable<T> headThree<T> (IEnumerable<T> source)
+        {
+            int i = 0;
+            foreach (T  element in source)
+            {
+                if (i < 3)
+                {
+                    i++;
+                    yield return element;
+                }
+            }
+        }
+
+        public static int difference (IEnumerable<int> source)
+        {
+            if (source.GetEnumerator().MoveNext())
+            {
+                int result = 0;
+                foreach (int element in source)
+                {
+                    result = result == 0 ? element : result - element;
+                }
+                return result;
+            }
+            else
+            {
+                throw new Exception("source cannot be empty");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> data = Enumerable.Range(12, 6);
+            IEnumerable<int> subData = EnumerableUtil.headThree<int>(data);
+            int difference = EnumerableUtil.difference(subData);
+            foreach(int element in subData)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine(string.Format("Difference of the head three element {0} ", difference.ToString()));
+
+            
+            Console.WriteLine("Extension Method");
+            IEnumerable<int> subData2 = data.headThree<int>();
+            foreach (int element in subData)
+            {
+                Console.WriteLine(element);
+            }
+
+            int difference2 = data.headThree<int>().difference();
+            Console.WriteLine(string.Format("Difference of the head three element {0} ", difference2.ToString()));
+            Console.Read();
+        }
+    }
+}
